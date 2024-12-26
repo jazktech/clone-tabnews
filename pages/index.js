e3760dea21faa07527c222acbd197c95b4843cfe
Jaaziel Lopes <159482894+jazktech@users.noreply.github.com>
2024-12-26 22:26:18 +0000

    adiciona os arquivos de manifesto e atualiza o index

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+function Home() {
+    // return <h1>Você é o amor da minha vida!</h1>
+    return <html>
+            <body>
+                <div>
+                    <h1 style={{ color: 'red' }}>&hearts;</h1>
+                    <h1>Você é o amor da minha vida!</h1>
+                </div>
+                <div>
+                    <br/>.<br/>.<br/>.<br/>.
+                    <br/>.<br/>.<br/>.<br/>.
+                    <br/>.<br/>.<br/>.<br/>.
+                    <br/>.<br/>.<br/>.<br/>.
+                    <br/>.<br/>.<br/>.<br/>.
+                    <br/>.<br/>.<br/>.<br/>.
+                    <br/>.<br/>.<br/>.<br/>
+                    <h7 style={{ color: 'white' }}>faz uma jantinha</h7>
+                    <h7 style={{ color: 'red' }}>&hearts; una os corações</h7>
+                </div>
+            </body>
+        </html>
+}
+
+export default Home;
